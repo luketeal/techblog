94b0f4f718a7f48a58b220fbf5b4edefc3918fdd
Luke Teal <l.teal21@gmail.com>
2021-06-12 22:27:43 -0400

    update user model and seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,23 +1,34 @@
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
+// const sequelize = require('../config/connection');
 
 const userData = [
   {
     name: "luke",
     email: "lteal@email.com",
-    password: "1234"
+    password: "12341234"
   },
   {
     name: "alex",
     email: "alex@email.com",
-    password: "1234"
+    password: "12341234"
   },
   {
     name: "pat",
     email: "pat@email.com",
-    password: "1234"
+    password: "12341234"
   },
 
 ];
+
+
+async function pwHash(userArray) {
+  for (const user of userArray) {
+    user.password = await bcrypt.hash(user.password, 10)
+  }
+}
+
+pwHash(userData)
 
 const seedUsers = () => User.bulkCreate(userData);
 
